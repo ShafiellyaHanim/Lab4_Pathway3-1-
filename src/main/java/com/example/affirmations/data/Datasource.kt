aa77ffd5b9d934877269b80f88f98a7732706bc7
ShafiellyaHanim <103558397+ShafiellyaHanim@users.noreply.github.com>
2022-11-25 13:10:32 +0800

    Add files via upload

---
+++ src/main/java/com/example/affirmations/data/Datasource.kt
@@ -0,0 +1,14 @@
+package com.example.affirmations.data
+
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+class Datasource() {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation10, R.drawable.image10)
+        )
+    }
+}
